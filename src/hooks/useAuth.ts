e345a7e550f3ef66a00bceb0c39d00078f710b37
01joanna <joa.cuevas9@gmail.com>
2026-03-09 17:30:31 +0100

    commit a vercel, cambio en lib/firebase

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,21 +1,12 @@
 import { onAuthStateChanged, User } from "firebase/auth";
 import { useEffect, useState } from "react";
-import { getAuthClient } from "@/lib/firebase";
+import { auth } from "@/lib/firebase";
 
 export const useAuth = () => {
     const [user, setUser] = useState<User | null>(null);
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        let auth;
-        try {
-            auth = getAuthClient();
-        } catch (e) {
-            console.warn("Firebase Auth solo funciona en el cliente");
-            setLoading(false);
-            return;
-        }
-
         const unsub = onAuthStateChanged(auth, (user) => {
             setUser(user);
             setLoading(false);
